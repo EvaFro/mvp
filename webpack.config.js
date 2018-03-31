98bacfa64645fd6d72be42f09cce2fa5b48c4f3b
EvaFro <frolli.erin@gmail.com>
2018-03-31 14:38:12 -0700

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,20 @@ var BUILD_DIR = path.resolve(dirname, 'client/public');
 var APP_DIR = path.resolve(dirname, 'client/src');
 
 var config = {
-entry: APP_DIR + '/index.jsx',
-output: {
-path: BUILD_DIR,
-filename: 'bundle.js'
-}
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
 };
 
 module.exports = config;
