cfa1a20a4072f399340fa9e65d65351c4abf85f7
EvaFro <frolli.erin@gmail.com>
2018-04-02 08:12:42 -0700

    add movie trailor, into paragraph & Marvel tag

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
+import CharacterList from './components/CharacterList'
 
 class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
+  render() {
+    return (
+      <div>
+        <p> Are you excited for the upcoming movie the Avengers: Infinity War!? But want a refesher of the all characters leading up to this multi-francise and multi-character movie! Marvel Character Geek Out Corner is your one stop page for learning the Marvel history of the top 30 charactors that are confermed to be in the film. We at Marvel Geek Out Corner hope you get your Marvel Geek on and enjoy this fun app! </p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/YZ1UN5gvLjQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <CharacterList />
+        <br/>
+        <br/>
+        <br/>
+        <div>Data provided by Marvel. © 2014 Marvel</div>
+      </div>
+    );
   }
 }
 
