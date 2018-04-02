db6f0c8bc5ed2fa3b448d448f5a7b5fbd795e482
EvaFro <frolli.erin@gmail.com>
2018-04-02 07:24:14 -0700

    fill out superHeroProfile comonent

--- client/src/components/SuperHeroProfile.jsx
+++ client/src/components/SuperHeroProfile.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 
-const SuperHeroProfile = () => (
+const SuperHeroProfile = (props) => (
   <div>
-  
+    <h3>{props.superHero.name}</h3>
+    <br/>
+    <div>Comics: {props.superHero.comics.available}</div>
+    <div>Series: {props.superHero.series.available}</div>
+    <div>Stories: {props.superHero.stories.available}</div>
+    <div>Events: {props.superHero.events.available}</div>
+    <div>Wiki: {props.superHero.urls[1].url}</div>
+    <br/>
+    <h4>About</h4>
+    <p>{props.superHero.description}</p>
+    <div></div>
+    <h4>Top 20 Comics</h4>
+    <div>
+      {props.superHero.comics.items.map(item => <li>{item.name}</li>)}
+    </div>
+    <h4>Top 20 Series</h4>
+    <div>
+      {props.superHero.series.items.map(item => <li>{item.name}</li>)}
+    </div>
+    <h4>Top 20 Stories</h4>
+    <div>
+      {props.superHero.stories.items.map(item => <li>{item.name}</li>)}
+    </div>
+    <h4>Top 20 Events</h4>
+    <div>
+      {props.superHero.events.items.map(item => <li>{item.name}</li>)}
+    </div>
   </div>
 );
 
