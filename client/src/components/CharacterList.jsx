6744ddeaf2b79297c5bc20173623080f4a615490
EvaFro <frolli.erin@gmail.com>
2018-04-02 08:17:27 -0700

    add a note to fill out state once data is seeded

--- client/src/components/CharacterList.jsx
+++ client/src/components/CharacterList.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import SuperHeroProfile from './SuperHeroProfile';
 import GridIcon from './GridIcon'
 
+// display the Character profile below the grid
+
 const CharacterList = (props) => (
   <div>
     {props.list.map(icon => <GridIcon icon={icon} onClick={props.displaySuperHero}/>)}
