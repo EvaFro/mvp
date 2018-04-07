a6f753b58ef85f4c1df270ce5d8216e141d9b8cd
EvaFro <frolli.erin@gmail.com>
2018-04-07 11:16:35 -0700

    change file path to public

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const HOST = parseInt(process.env.HOST) || 'localhost';
 
 var app = express();
 
-app.use(express.static(__dirname + '/../client/public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.listen(PORT, function() {
   console.log(`listening on port: ${HOST}:${PORT}!`);
