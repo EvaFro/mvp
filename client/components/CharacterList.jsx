27452abaaba2b035821b2a3939cc63bb2cdcc0bb
EvaFro <frolli.erin@gmail.com>
2018-04-07 14:46:13 -0700

    reorginize to add css styling later

--- client/components/CharacterList.jsx
+++ client/components/CharacterList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import SuperHeroProfile from './SuperHeroProfile.jsx';
-import GridIcon from './GridIcon.jsx'
-import styles from './styles.css'
+import SuperHeroProfile from './SuperHeroProfile';
+import GridIcon from './GridIcon'
+import styles from './styles'
 // display the Character profile below the grid
 
 const CharacterList = (props) => (
   <div className ="char-list">
-    {props.superHeroList.map(superHero => <GridIcon />)}
+    {props.superHeroList.map(superHero => <GridIcon superHero={superHero}/>)}
   </div>
   
 );
